05adef8343e8d7648982f9457f8afe867db2314d
Nvieitez <nvieitezdominguez@danielcastelao.org>
2023-10-02 08:58:09 +0200

    Entrega Boletín 2

---
+++ Boletines/Boletín02/Boletín2_1/src/boletín2_1/Boletín2_1.java
@@ -0,0 +1,19 @@
+package boletín2_1;
+
+public class Boletín2_1 {
+
+    public static void main(String[] args) {
+       
+// Datos triángulo
+        float base=4,altura=3; 
+
+// Proceso de calculo del area      
+        float area=base*altura/2;
+              
+// Resultado
+            System.out.println("area del triángulo = "+area);
+            
+    }
+    
+}
+
---
+++ Boletines/Boletín02/Boletín2_2/src/boletín2_2/Boletín2_2.java
@@ -0,0 +1,18 @@
+package boletín2_2;
+
+public class Boletín2_2 {
+
+   public static void main(String[] args) {
+     
+// Datos cuadrado
+       float lado=3; 
+             
+// Proceso de calculo del area
+       float area=lado*lado;
+ 
+// Resultado
+            System.out.println("area del cuadrado = "+area);
+            
+    }
+    
+}
